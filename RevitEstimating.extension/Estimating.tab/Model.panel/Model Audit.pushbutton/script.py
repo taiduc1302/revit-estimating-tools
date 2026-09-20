1d1588cad6aa900f12db0f962435973c33c14570
Mickey Vu <38831891+taiduc1302@users.noreply.github.com>
2026-09-20 04:53:46 -0700

    refactor: use pyRevit extension lib path

--- RevitEstimating.extension/Estimating.tab/Model.panel/Model Audit.pushbutton/script.py
+++ RevitEstimating.extension/Estimating.tab/Model.panel/Model Audit.pushbutton/script.py
@@ -1,15 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
 
-import os
-import sys
-
 from pyrevit import forms, revit, script
-
-ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", "..", ".."))
-LIB = os.path.join(ROOT, "lib")
-if LIB not in sys.path:
-    sys.path.insert(0, LIB)
 
 from revit_estimating.audit import audit_model, audit_summary
 from revit_estimating.revit_adapter import extract_model
